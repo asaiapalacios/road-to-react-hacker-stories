5ae4eeb142060c6b64776d2c0a8bb34e6fec4b77
Asaia <14948113+asaiapalacios@users.noreply.github.com>
2022-04-04 18:09:19 -0500

    Move stories out of global scope; Use props

--- src/App.js
+++ src/App.js
@@ -1,42 +1,43 @@
 import * as React from "react";
 
-const list = [
-  {
-    title: "React",
-    url: "https://reactjs.org/",
-    author: "Jordan Walke",
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: "Redux",
-    url: "https://redux.js.org/",
-    author: "Dan Abramov, Andrew Clark",
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
+function App() {
+  const stories = [
+    {
+      title: "React",
+      url: "https://reactjs.org/",
+      author: "Jordan Walke",
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: "Redux",
+      url: "https://redux.js.org/",
+      author: "Dan Abramov, Andrew Clark",
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
 
-// arrow function with a concise body (implicit return)
-const App = () => (
-  <div>
-    <h1>My Hacker Stories</h1>
+  return (
+    <div>
+      <h1>My Hacker Stories</h1>
+      {/* an instance of Search component */}
+      <Search />
 
-    <Search />
+      <hr />
 
-    <hr />
+      {/* an instance of List component */}
+      <List list={stories} />
+    </div>
+  );
+}
 
-    {/* an instance of List component */}
-    <List />
-  </div>
-);
-
-const Search = () => {
+function Search() {
   // This event handler function:
-  // -receives the event object from user interaction (typed input);
-  // -accesses through the object passed for the emitted value: event.target.value (the input field value)
+  // -receives the event object from user interaction/event change triggered when user typed input value;
+  // -accesses the object passed, specifically, targeting the emitted value: event.target.value (the input field value)
   // -> the emitted value is logged to the console
   const handleChange = (event) => {
     return console.log(event.target.value);
@@ -45,25 +46,36 @@ const Search = () => {
   return (
     <div>
       <label htmlFor="search">Search: </label>
+      {/* handleChange is a reference to the function */}
       <input id="search" type="text" onChange={handleChange} />
     </div>
   );
-};
+}
 
-// arrow function with a concise body (implicit return)
-const List = () => (
-  <ul>
-    {list.map((item) => (
-      <li key={item.objectID}>
-        <span>
-          <a href={item.url}>{item.title}: </a>
-        </span>
-        <span>{item.author}; </span>
-        <span>{item.num_comments}, </span>
-        <span>{item.points}</span>
-      </li>
-    ))}
-  </ul>
-);
+// Render list
+function List(props) {
+  return (
+    <ul>
+      {props.list.map((item) => (
+        <Item key={item.objectID} item={item} />
+      ))}
+    </ul>
+  );
+}
+
+// Pull our item into its own component
+// Each item is focused on what the items inside of that list looks like
+function Item(props) {
+  return (
+    <li>
+      <span>
+        <a href={props.item.url}>{props.item.title}</a>
+      </span>
+      <span>{props.item.author}</span>
+      <span>{props.item.num_comments}</span>
+      <span>{props.item.points}</span>
+    </li>
+  );
+}
 
 export default App;
